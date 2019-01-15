c376dc91dbad9b8f7d6e635ee3b32e2b57da65df
Milo Anderson <themiloanderson@gmail.com>
2019-01-15 10:35:29 -0800

    added README

--- server.js
+++ server.js
@@ -20,13 +20,17 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 function searchToLatLong(query) {
   const geoData = require('./data/geo.json');
-  const location = new Location(geoData);
+  const location = new Location(geoData, query);
   location.search_query = query;
   return location;
 }
 
-function Location(data) {
-  this.formatted_query = data.results[0].formatted_address;
-  this.latitude = data.results[0].geometry.location.lat;
-  this.longitude = data.results[0].geometry.location.lng;
+function Location(data, query) {
+  for (let val in data.results) {
+    if (data.results[val].address_components[0].long_name.toLowerCase() === query.toLowerCase()) {
+      this.formatted_query = data.results[0].formatted_address;
+      this.latitude = data.results[0].geometry.location.lat;
+      this.longitude = data.results[0].geometry.location.lng;
+    }
+  }
 }
